8431ebbc2aac6a9a89ddc2096784ad83258c4409
msprijatelj <44978587+msprijatelj@users.noreply.github.com>
2023-05-18 22:17:30 -0400

    update version tag to 0.0.2

---
+++ create_tag.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+tagName=$1;
+
+if [[ -z "${tagName}" ]]; then
+    exit 1
+fi
+
+update_version () {
+    local jsonFile=$1
+    node > out_${jsonFile} <<EOF
+    var data = require('./${jsonFile}');
+    data.version = '${tagName}';
+    console.log(JSON.stringify(data, null, 2));
+
+EOF
+    rm ${jsonFile}
+    mv out_${jsonFile} ${jsonFile}
+}
+
+update_version package.json
+cd lib/
+update_version lcp_manifest.json
+cd ../
+
+git add package.json lib/lcp_manifest.json
+git commit -m "update version tag to ${tagName}"
+
+read -p "Enter tag message: " msg
+git tag -s "${tagName}" -m "${msg}"
+git push origin
+git push origin "${tagName}"
+
